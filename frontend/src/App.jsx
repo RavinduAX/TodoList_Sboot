6ca37615afe648aa9607d86dc6a5a509d86d81a5
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-08-31 19:27:20 +0530

    content updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className='w-screen h-screen flex justify-center '>
+      <div className=' bg-white p-8 rounded-lg w-2/5 h-max relative top-32'>
+        <h1 className='font-semibold text-2xl mb-6'>Things Todo 📝</h1>
+        
+      </div>
     </div>
   );
 }
